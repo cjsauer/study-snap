3d008751a390ce5167f631b024053948330f38ce
Elliott Mantock <emantock@gmail.com>
2015-06-01 10:57:19 -0400

    added basic quick form

--- collections/courses.js
+++ collections/courses.js
@@ -1,4 +1,32 @@
+var CourseSchema = new SimpleSchema({
+  university: {
+    type: String,
+    label: "School",
+    max: 300
+  },
+  title: {
+    type: String,
+    label: "Course name"
+  },
+  deck: {
+    type: Number,
+    label: "Deck ID",
+    /*
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Number();
+      } else {
+        this.unset();
+      }
+    },*/
+    index: true,
+    unique: true
+  }
+});
+
 Courses = new Meteor.Collection('courses');
+
+Courses.attachSchema(CourseSchema);
 
 Courses.initEasySearch(['university', 'title'], {
   'limit': 10,
