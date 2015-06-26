837d0c2fa5e889305d71fdf21aa03bbb6c06b79a
Elliott Mantock <emantock@gmail.com>
2015-06-26 14:20:34 -0400

    can now see index on each card

--- client/views/flashcards/Flashcard.js
+++ client/views/flashcards/Flashcard.js
@@ -4,3 +4,9 @@ Template.Flashcard.events({
     template.$('.back').toggleClass('hidden');
   }
 });
+
+UI.registerHelper('addIndex', function (all) {
+    return _.map(all, function(val, index) {
+        return {index: index, value: val};
+    });
+});
