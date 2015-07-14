3f8ca6741b83ed4d87aa7724524f96bb904ecfc1
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-07-14 17:05:42 -0400

    Sort the leaderboard by score

--- client/views/courses/Leaderboard.js
+++ client/views/courses/Leaderboard.js
@@ -2,7 +2,11 @@ Template.Leaderboard.helpers({
   students : function() {
     // We are subscribed to course in the route file
     var course = Courses.find().fetch()[0];
-    return course.students;
+    return course.students.sort(function(a, b) {
+      if(a.score < b.score) return 1;
+      if(a.score > b.score) return -1;
+      return 0;
+    });
   }
 });
 
