1ebaeff5da99786f50c17d887825527fdb209fb0
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-07-14 13:53:27 -0400

    Fixed up the new card form a bit

--- client/views/flashcards/Flashcard.js
+++ client/views/flashcards/Flashcard.js
@@ -4,15 +4,3 @@ Template.Flashcard.events({
     template.$('.back').toggleClass('hidden');
   }
 });
-
-Template.Flashcard.helpers({
-  indexIsZero: function() {
-    return this.index == 0
-  }
-});
-
-UI.registerHelper('addIndex', function (all) {
-    return _.map(all, function(val, index) {
-        return {index: index, value: val};
-    });
-});
