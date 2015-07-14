e749370af386695f2d04a10593a292755ca1332e
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-07-14 16:25:37 -0400

    Simplified the leaderboard pub/sub

--- client/views/courses/Leaderboard.js
+++ client/views/courses/Leaderboard.js
@@ -1,6 +1,7 @@
 Template.Leaderboard.helpers({
   students : function() {
-    // We are subscribed to courseStudents in the route file
-    return Meteor.users.find().fetch();
+    // We are subscribed to course in the route file
+    var course = Courses.find().fetch()[0];
+    return course.students;
   }
 });
--- server/publications/courses.js
+++ server/publications/courses.js
@@ -8,15 +8,3 @@ Meteor.publish('courses', function() {
 Meteor.publish('course', function(id) {
   return Courses.find(id);
 });
-
-// The students of a course
-Meteor.publish('courseStudents', function(id) {
-  var course = Courses.find(id);
-  var studentIDs = course && course.students;
-
-  if(studentIDs) {
-    return Meteor.users.find({_id: {$in: studentIDs} });
-  } else {
-    return [];
-  }
-});
