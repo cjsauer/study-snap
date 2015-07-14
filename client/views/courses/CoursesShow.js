add46fa100770e74ce31258d1d360af51c09b77e
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-07-14 17:02:01 -0400

    Confirm course leaving with a dialog

--- client/views/courses/CoursesShow.js
+++ client/views/courses/CoursesShow.js
@@ -21,12 +21,15 @@ Template.CoursesShow.events({
 
   'click #leaveCourseButton': function(event) {
     event.preventDefault();
-    var routerParams = Router.current().params;
-    var courseID = routerParams && routerParams._id;
-    Meteor.call('leaveCourse', courseID, function(err, result) {
-      if(err) {
-        console.error(err);
-      }
-    });
+
+    if(confirm("Are you sure you want to leave this course?")) {
+      var routerParams = Router.current().params;
+      var courseID = routerParams && routerParams._id;
+      Meteor.call('leaveCourse', courseID, function(err, result) {
+        if(err) {
+          console.error(err);
+        }
+      });
+    }
   }
 });
