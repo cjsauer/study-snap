6331a29aaeb450686851bf4d1c56e00590ea1a8a
Elliott Mantock <emantock@gmail.com>
2015-08-05 10:28:24 -0400

    fix styles and set clicked buttons active

--- client/views/challenge/Challenge.js
+++ client/views/challenge/Challenge.js
@@ -1,6 +1,16 @@
 Template.Challenge.helpers({
   cards: function() {
     return _.sample(Flashcards.find().fetch(), 5);
+  },
+  response: function() {
+    _.shuffle([response1, response2, response3, response4]);
+  }
+});
+Template.ChallengeQuestion.events({
+  "click .list-group-item": function(event, template){
+    //template.$('.list-group-item').toggleClass('active');
+    template.$('.list-group-item').not(event.target).removeClass('active');
+    template.$(event.target).toggleClass('active');
   }
 });
 /*
