0ccedb00e83707f66d246df6f73e0edc7dacd1a7
Elliott Mantock <emantock@gmail.com>
2015-08-05 16:12:46 -0400

    users that create a course automatically join it

--- client/views/courses/CoursesNew.js
+++ client/views/courses/CoursesNew.js
@@ -1,6 +1,11 @@
 AutoForm.hooks({
   'insertCourseForm': {
     onSuccess: function(formType, result) {
+      Meteor.call('joinCourse', result, function(err, result) {
+        if(err) {
+          console.error(err);
+        }
+      });
       Router.go('courses.show', {_id: result});
     }
   }
