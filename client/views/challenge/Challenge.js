207254f51186305d2fbafe9894b966860334c090
Elliott Mantock <emantock@gmail.com>
2015-08-05 09:59:44 -0400

    added response buttons

--- client/views/challenge/Challenge.js
+++ client/views/challenge/Challenge.js
@@ -3,5 +3,16 @@ Template.Challenge.helpers({
     return _.sample(Flashcards.find().fetch(), 5);
   }
 });
+/*
 Template.ChallengeQuestion.helpers({
+  response: function() {
+    _.shuffle([response1, response2, response3, response4]);
+  }
 });
+*/
+/*
+var ul = document.querySelector('ul');
+for (var i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[Math.random() * i | 0]);
+}
+*/
