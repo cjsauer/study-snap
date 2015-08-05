3c41df387b08f3317dce72baea22af12e08a40bb
Elliott Mantock <emantock@gmail.com>
2015-08-05 15:36:26 -0400

    added challenges collection

---
+++ server/permissions/challenges.js
@@ -0,0 +1,13 @@
+Challenges.allow({
+  insert: function(userId, doc) {
+    return Roles.userIsInRole(userId, 'user');
+  },
+
+  update: function(userId, doc, fields, modifier) {
+    return false;
+  },
+
+  remove: function(userId, doc) {
+    return false;
+  }
+});
