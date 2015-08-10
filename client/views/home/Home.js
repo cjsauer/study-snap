a8c97e9b88c4da97a29de9a38da22d05d76525c1
Elliott Mantock <emantock@gmail.com>
2015-08-10 11:19:51 -0400

    added courses and challenges to home page

--- client/views/home/Home.js
+++ client/views/home/Home.js
@@ -1,7 +1,13 @@
 Template.Home.helpers({
-  challenges: function() {
+  courses: function() {
+    return Courses.find({"students.id": Meteor.userId()}).fetch();
+  }
+});
+Template.MyCourse.helpers({
+  challenges: function(courseId) {
     return _.map(Challenges.find({"challengee": Meteor.userId()}).fetch(), function(challenge) { 
-      var course = Courses.findOne({"_id": challenge.course});
+      var course = Courses.findOne({"_id": courseId});
+      console.log(course);
       return {
         "course": course.title, 
         "student": _.find(course.students, function(student) { 
