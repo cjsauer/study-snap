96af8cb34eb39d11b51845e1bf0dee74bf329fd6
Elliott Mantock <emantock@gmail.com>
2015-08-12 09:58:23 -0400

    add comments and verify user based on name too

--- server/hooks/onCreateUser.js
+++ server/hooks/onCreateUser.js
@@ -8,15 +8,21 @@ Accounts.onCreateUser(function(options, user) {
   user.roles = {};
   user.roles[Roles.GLOBAL_GROUP] = ['user'];
 
-  var service = "";
+  // The remaining code will attach multiple services to users
+  // This may only work for google and facebook
+  var service;
   if (user.services) {
     service = _.keys(user.services)[0];
   }
 
+  // get name and email of user
   var email = user.services[service].email;
+  var name = user.services[service].name;
   var oldUser;
-  if (email) {
-    oldUser = Meteor.users.findOne({"emails.address": email});
+  // potential flaw if a user uses different email addresses for his/her services
+  if (email && name) {
+    // find a user with matching name and email
+    oldUser = Meteor.users.findOne({"emails.address": email, "profile.name": name});
   }
 
   if (oldUser) {
@@ -24,14 +30,17 @@ Accounts.onCreateUser(function(options, user) {
       oldUser.services = {};
     }
 
+    // replace the user with one that has the same userId but
+    //  also has a new service attached to it
     if (service === "google" || service === "facebook") {
       oldUser.services[service] = user.services[service];
       Meteor.users.remove(oldUser._id);
       user = oldUser;
     }
-  } else {
+  } else {    // if the user was not found, add a new user with that email and name
     if (service === "google" || service === "facebook") {
       if (user.services[service].email) {
+        // add a new field to the user in the collection
         user.emails = [{address: user.services[service].email, verified: true}];
       } else {
         throw new Meteor.Error(500, service + "account has no email attached");
